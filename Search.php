81eb496c947f794b537ea896e026bfae6cb2a55f
BoBrebel <mohamedelfadhel.elhachemi@esprit.tn>
2015-05-26 13:42:40 +0100

    fix

--- Search.php
+++ Search.php
@@ -3,6 +3,7 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Youth Decides</title>
+	<link rel="stylesheet" href="style.css">
 	<!-- Latest compiled and minified CSS -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
 
@@ -11,12 +12,13 @@
 
 	<!-- Latest compiled and minified JavaScript -->
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
-	<link rel="stylesheet" href="style.css">
+	
 </head>
 <body>
 	<section id="search">
 		<div class="container">
-		 <input type="text" class="form-control" placeholder="Your search here">
+			<img src="images/Youth Decides.png">
+			<input type="text" class="form-control" placeholder="Search for member">
 		</div>
 	</section>
 	
